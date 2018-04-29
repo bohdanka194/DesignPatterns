21aa268f28eac55da7e33d73d39e5c102f903fad
bohdanka194 <bohdanka194@gmail.com>
2018-04-29 17:23:30 +0300

    Add Singelton

---
+++ Singelton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer comp = new Computer();
+            comp.Launch("Windows 8.1");
+            Console.WriteLine(comp.OS.Name);
+
+            // у нас не получится изменить ОС, так как объект уже создан    
+            comp.OS = OperationSystem.getInstance("Windows 10");
+            Console.WriteLine(comp.OS.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
