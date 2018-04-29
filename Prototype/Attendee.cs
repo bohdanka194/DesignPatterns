a420cbb156f44c09a4a7d6d92cb62d715b99daf8
bohdanka194 <bohdanka194@gmail.com>
2018-04-29 16:09:29 +0300

    Add Prototype design pattern

---
+++ Prototype/Attendee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Prototype
+{
+    public class Attendee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+        //Attendee() { }
+        //Attendee(string firstName,string lastName)
+        //{
+        //    FirstName = firstName;
+        //    LastName = lastName;
+        //}
+    }
+}
---
+++ Prototype/Priority.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Prototype
+{
+    public class Priority: ICloneable
+    {
+        private int _priority;
+
+        private Priority(int priority)
+        {
+            _priority = priority;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public static Priority High()
+        {
+            return new Priority(1);
+        }
+
+        public static Priority Low()
+        {
+            return new Priority(-1);
+        }
+
+        public void SetPriorityValue(int priority)
+        {
+            _priority = priority;
+        }
+
+        public bool IsHigh()
+        {
+            return _priority > 0;
+        }
+    }
+}
