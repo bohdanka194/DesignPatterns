d1863c0e4c52a55e851fb4f31c1118454a70b152
bohdanka194 <bohdanka194@gmail.com>
2018-05-01 11:49:34 +0300

    Add Composite (example:FileSystem)

---
+++ Composite/Directory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Directory : Component
+    {
+        private List<Component> components = new List<Component>();  
+        public Directory(string name) : base(name)
+        {
+        }
+
+        public override void Add(Component component)
+        {
+            components.Add(component);
+        }
+        public override void Remove(Component component)
+        {
+            components.Remove(component);
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Node " + name);
+            Console.WriteLine("Subnodes:");
+            for( int i = 0; i < components.Count; i++)
+            {
+                components[i].Print();
+            }
+        }
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Component fileSystem = new Directory("File System");
+            Component diskC = new Directory("Disk C");
+            Component pngFile = new File("12345.png");
+            Component docxFile = new File("DataMining.docx");
+
+            diskC.Add(pngFile);
+            diskC.Add(docxFile);
+
+            fileSystem.Add(diskC);
+
+            fileSystem.Print();
+            Console.WriteLine();
+
+            //diskC.Remove(pngFile);
+
+            Component docFolder = new Directory("My documents");
+            Component txtFile = new File("readme.txt");
+            Component csFile = new File("Program.cs");
+
+            docFolder.Add(txtFile);
+            docFolder.Add(csFile);
+
+            diskC.Add(docFolder);
+
+            fileSystem.Print();
+
+            Console.Read();
+        }
+    }
+}
