7ddb7dceda42ae7ad9ae3020f0de2e4cd0f3bb7f
bohdanka194 <bohdanka194@gmail.com>
2018-05-07 21:31:37 +0300

    Add Proxy

---
+++ Proxy/BookStore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class BookStore :IBook
+    {
+        PageContext db;
+        public BookStore()
+        {
+            db = new PageContext();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public Page GetPage(int number)
+        {
+            return db.Pages.FirstOrDefault(p => p.Number == number);
+        }
+    }
+}
---
+++ Proxy/PageContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Data.Entity;
+namespace Proxy
+{
+    class PageContext : DbContext
+    {
+        public DbSet<Page> Pages { get; set; }
+    }
+}
