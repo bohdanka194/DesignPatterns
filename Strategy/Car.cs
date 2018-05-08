d9c168e8e243ca493e7ac6a7b11f849c5877e3b4
bohdanka194 <bohdanka194@gmail.com>
2018-05-08 13:56:52 +0300

    Add Strategy

---
+++ Strategy/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Car
+    {
+        protected int passengers;
+        protected string model;
+        public IMovable Movable { private get; set; }
+        public Car (int pass, string mod,IMovable movable)
+        {
+            passengers = pass;
+            model = mod;
+            Movable = movable;
+        }
+        public void Move()
+        {
+            Movable.Move();
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(4, "VW Golf 5", new PetrolMove());
+            car.Move();
+            car.Movable = new ElectricMove();
+            car.Move();
+
+            Console.Read();
+        }
+    }
+}
