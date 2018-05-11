ace627bfa8a95f8587d0b2ed2155200da4f036f4
bohdanka194 <bohdanka194@gmail.com>
2018-05-11 18:24:12 +0300

    Add TemplateMethod

---
+++ TemplateMethod/School.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    class School : Education
+    {
+        public override void Enter()
+        {
+            Console.WriteLine("Enter a school.");
+        }
+        public override void Study()
+        {
+            Console.WriteLine("Visit school and do homework");
+        }
+        public override void GetDocument()
+        {
+            Console.WriteLine("Get certification from high school");
+        }
+    }
+}
---
+++ TemplateMethod/University.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    class University: Education
+    {
+        public override void Enter()
+        {
+            Console.WriteLine("Enter a university");
+        }
+        public override void Study()
+        {
+            Console.WriteLine("Visit lectures");
+            Console.WriteLine("Practice!");
+        }
+        public override void GetDocument()
+        {
+            Console.WriteLine("Get diploma while graduated");
+        }
+    }
+}
