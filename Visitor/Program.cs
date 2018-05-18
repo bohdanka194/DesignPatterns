89d652e175a9e703ecd10bb3746b944c9e5b9808
bohdanka194 <bohdanka194@gmail.com>
2018-05-18 13:49:12 +0300

    Add Visitor

---
+++ Visitor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var structure = new Bank();
+            structure.Add(new Person { Name = "Bohdanka", Number = "82184931" });
+            structure.Add(new Company { Name = "Microsoft", RegNumber = "ewuir32141324", Number = "3424131445" });
+            structure.Accept(new HtmlVisitor());
+            Console.WriteLine();
+            structure.Accept(new XmlVisitor());
+
+            Console.Read();
+        }
+    }
+}
---
+++ Visitor/XmlVisitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class XmlVisitor : IVisitor
+    {
+        public void VisitPersonAcc(Person acc)
+        {
+            string result = "<Person><Name>" + acc.Name + "</Name>" +
+                "<Number>" + acc.Number + "</Number><Person>";
+            Console.WriteLine(result);
+        }
+
+        public void VisitCompanyAc(Company acc)
+        {
+            string result = "<Company><Name>" + acc.Name + "</Name>" +
+                "<RegNumber>" + acc.RegNumber + "</RegNumber>" +
+                "<Number>" + acc.Number + "</Number><Company>";
+            Console.WriteLine(result);
+        }
+    }
+}
