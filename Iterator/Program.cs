043cd0f08785317453563bedab2ef666c9e6daaf
bohdanka194 <bohdanka194@gmail.com>
2018-05-18 12:48:24 +0300

    change Iterator, add State

--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -10,11 +10,33 @@ namespace Iterator
     {
         static void Main(string[] args)
         {
-            Library library = new Library();
-            Reader reader = new Reader();
-            reader.SeeBook(library);
+            //Library library = new Library();
+            //Reader reader = new Reader();
+            //reader.SeeBook(library);
+            Derived d = new Derived();
+            d.Print();
+            Base bd = new Derived();
+            bd.Print();
+            Base b = new Base();
+            b.Print();
 
+            var list = new List<int>() { 3, 4, 5, 10 };
+            Console.WriteLine(list.FirstOrDefault(i => i>3));
             Console.Read();
+        }
+    }
+    class Base
+    {
+        public void Print()
+        {
+            Console.WriteLine($"calling from {this.GetType().Name}");
+        }
+    }
+    class Derived: Base
+    {
+        public new void Print()
+        {
+            Console.WriteLine($"calling from {this.GetType().Name}");
         }
     }
 }
---
+++ State/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Water water = new Water(WaterState.LIQUID);
+            water.Head();
+            water.Head();
+            water.Frost();
+            water.Frost();
+            Console.ReadLine();
+        }
+    }
+}
