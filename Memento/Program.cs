1b8f033f9c51578a8962530f26351c1b03b08b58
bohdanka194 <bohdanka194@gmail.com>
2018-05-18 13:39:10 +0300

    add Memento

---
+++ Memento/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Hero();
+            hero.Shoot();
+            GameHistory game = new GameHistory();
+
+            game.History.Push(hero.SaveState());
+            hero.Shoot(); 
+            hero.RestoreState(game.History.Pop());
+            hero.Shoot(); 
+
+            Console.Read();
+        }
+    }
+}
