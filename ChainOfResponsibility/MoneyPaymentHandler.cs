34321d67108a053154790ad2f30a436cabf6ed48
bohdanka194 <bohdanka194@gmail.com>
2018-05-18 12:55:13 +0300

    add ChainOfResponsibility

---
+++ ChainOfResponsibility/MoneyPaymentHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class MoneyPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.MoneyTransfer == true)
+                Console.WriteLine("Выполняем перевод через системы денежных переводов");
+            else if (Successor != null)
+                Successor.Handle(receiver);
+        }
+    }
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Receiver receiver = new Receiver(false, true, true);
+
+            PaymentHandler bankPaymentHandler = new BankPaymentHandler();
+            PaymentHandler moneyPaymentHnadler = new MoneyPaymentHandler();
+            PaymentHandler paypalPaymentHandler = new PayPalPaymentHandler();
+            bankPaymentHandler.Successor = paypalPaymentHandler;
+            paypalPaymentHandler.Successor = moneyPaymentHnadler;
+
+            bankPaymentHandler.Handle(receiver);
+
+            Console.Read();
+        }
+    }
+}
